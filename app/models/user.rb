6d355d7c1399c6b43afc312c97384f07df03ea70
github username <meliajm@gmail.com>
2020-12-02 16:38:00 -0600

    fixed user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,4 @@
 class User < ApplicationRecord
-    validates_presence_of 
-    :firstname,
-    :lastname,
-    :email,
-    :phonenumber,
-    :receivesNotification,
-    :address1,
-    :address2,
-    :city,
-    :zip,
-    :state,  
-    :message,
-    :canVolunteer
+    validates_presence_of :firstname, :lastname, :email, :phonenumber, :receivesNotification, :address1, :address2, :city, :zip, :state, :message, :canVolunteer, :password
+    
 end
