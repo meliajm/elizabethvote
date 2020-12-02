bc3d9dd75f9680641d2a5d97435cd5f20a8c9e21
github username <meliajm@gmail.com>
2020-12-02 13:16:35 -0600

    changing gets to posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do 
-      resources :users
+      # resources :users
+      post '/users/index', to: 'users#index'
+      post '/user/:id', to: 'users#show'
+      post '/users', to: 'users#create'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
