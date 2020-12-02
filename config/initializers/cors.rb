f27ab011b6271bbf08f433aa11083b3a96ae0004
github username <meliajm@gmail.com>
2020-12-02 16:58:11 -0600

    cors edit

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://boring-dijkstra-99867a.netlify.app'
+    origins 'https://boring-dijkstra-99867a.netlify.app', '*'
 
     resource '*',
       headers: :any,
